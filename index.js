760457c733204c6de6d7d1fb9b7d4dd021b7ffb3
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2022-12-04 20:40:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Write your code here!
 main.remove() 
 let newHeader = document.querySelector('h1')
+function updateHeader(header){
+    newHeader.innerHTML = header
+}
+updateHeader('Griffins is the champion')
